48ef974f79618b9fb886c2b42f04099d9533866a
kwelsh1 <kwelsh1@uw.edu>
2022-11-28 21:16:20 -0800

    upload

--- src/main/resources/create.sql
+++ src/main/resources/create.sql
@@ -16,3 +16,18 @@ CREATE TABLE Vaccines (
     Doses int,
     PRIMARY KEY (Name)
 );
+
+CREATE TABLE Patients (
+    Username varchar(255),
+    Salt BINARY(16),
+    Hash BINARY(16),
+    PRIMARY KEY (Username)
+);
+
+CREATE TABLE Appointments (
+    appointment_id int,
+    Time date,
+    Caregiver varchar(255) REFERENCES Caregivers,
+    Patient varchar(255) REFERENCES Patients,
+    PRIMARY KEY (appointment_id)
+);
